68f7372e61fcc786cde92e30760e8c396cb9a6a0
Reza Goodarzi <Rezagoodarzi026@gmail.com>
2022-08-07 14:05:07 +0430

    build footer ui

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { footerList1, footerList2, footerList3 } from "../utils/constants";
+
+const List = ({ items, mt }: { items: string[]; mt: boolean }) => (
+  <div className={`flex flex-wrap gap-2 ${mt && "mt-5"}`}>
+    {items.map((item) => (
+      <p className="text-gray-400 text-sm hover:underline cursor-pointer" key={item}>
+        {item}
+      </p>
+    ))}
+  </div>
+);
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-6 hidden xl:block">
+      <List items={footerList1} mt={false} />
+      <List items={footerList2} mt={true} />
+      <List items={footerList3} mt={true} />
+      <p className="text-gray-400 text-sm mt-5">2022 TikTok Clone</p>
+    </div>
+  );
 };
 
 export default Footer;
