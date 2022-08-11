96ff97b9c0a8815819f8b777600b695123ba7a7e
Reza Goodarzi <Rezagoodarzi026@gmail.com>
2022-08-11 14:29:51 +0430

    Fix bugs

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import Link from "next/link";
-// import GoogleLogin from "react-google-login";
 import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import { ImCancelCircle } from "react-icons/im";
 import Discover from "./Discover";
@@ -11,8 +10,6 @@ import Footer from "./Footer";
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true);
-
-  const userProfile = false;
 
   const normalLink =
     "flex items-center gap-3 hover:bg-primary p-3 justify-start cursor-pointer font-semibold text-[#F51997] rounded";
@@ -37,28 +34,7 @@ const Sidebar = () => {
               </div>
             </Link>
           </div>
-          {!userProfile && (
-            <div className="px-2 py-4 hidden xl:block">
-              <p className="text-gray-400">Login to like and comment on videos</p>
-              <div className="pr-4">
-                {/* <GoogleLogin
-                  clientId=""
-                  render={(renderProps) => (
-                    <button
-                      className="cursor-pointer bg-white text-lg text-[#F51997] border-[1px] border-[#F51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#F51997]"
-                      onClick={renderProps.onClick}
-                      disabled={renderProps.disabled}
-                    >
-                      Login
-                    </button>
-                  )}
-                  onSuccess={() => {}}
-                  onFailure={() => {}}
-                  cookiePolicy="single_host_origin"
-                /> */}
-              </div>
-            </div>
-          )}
+
           <Discover />
           <SuggestedAccounts />
           <Footer />
