a038406f2af295b2594bdf7ad7065515feec2ab3
Reza Goodarzi <Rezagoodarzi026@gmail.com>
2022-08-13 22:31:34 +0430

    filter videos base on selected topics

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,12 +22,17 @@ const Home: NextPage<IProps> = ({ videos }) => {
   );
 };
 
-export const getServerSideProps = async () => {
-  const { data } = await axios.get(`${BASE_URL}/api/post`);
+export const getServerSideProps = async ({ query: { topic } }: { query: { topic: string } }) => {
+  let response = null;
 
+  if (topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  } else {
+    response = await axios.get(`${BASE_URL}/api/post`);
+  }
   return {
     props: {
-      videos: data,
+      videos: response.data,
     },
   };
 };
